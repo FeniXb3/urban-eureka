fdf13264feb2d22ab66871f84b91955affdb77c6
Konrad Gadzina <konrad@makegames.today>
2024-05-07 12:12:51 +0200

    Add different cell types

--- UrbanEureka/Map.cs
+++ UrbanEureka/Map.cs
@@ -4,28 +4,36 @@ public class Map
     public Point Origin { get; set;}
     private int[][] mapData;
     private Dictionary<int, char> cellVisuals = new Dictionary<int, char>{
-        { 1, '#'},
+        { 1, '+'},
+        { 2, '-'},
+        { 3, '|'},
         { 0, '.'},
         { 9, ' '},
+        { 4, '#'},
     };
 
     private Dictionary<int, ConsoleColor> colorMap = new Dictionary<int, ConsoleColor> {
         { 1, ConsoleColor.DarkBlue},
+        { 2, ConsoleColor.DarkBlue},
+        { 3, ConsoleColor.DarkBlue},
         { 0, ConsoleColor.Magenta},
         { 9, ConsoleColor.Black},
+        { 4, ConsoleColor.Green},
     };
+
+    private int[] walkableCellTypes = new int[] { 0, 4 };
 
     public Map()
     {
         mapData = new int[][] {
-            new []{1,1,1,1,1,1,1,1,1,1,1,1,},
-            new []{1,0,0,0,1,1,1,1,0,0,0,1,},
-            new []{1,0,0,0,0,0,0,0,1,0,0,1,},
-            new []{1,1,1,1,1,1,1,0,1,0,0,1,},
-            new []{9,9,9,1,0,0,0,0,1,0,0,1,},
-            new []{9,9,9,1,0,0,0,1,1,0,0,1,},
-            new []{9,9,9,1,0,0,0,0,0,0,0,1,},
-            new []{9,9,9,1,1,1,1,1,1,1,1,1,},
+            new []{1,2,2,2,1,9,9,1,2,2,2,1,},
+            new []{3,0,0,0,1,2,2,1,0,0,0,3,},
+            new []{3,0,0,0,0,0,0,0,1,0,0,3,},
+            new []{1,2,2,1,2,2,1,4,3,0,4,3,},
+            new []{9,9,9,3,0,0,0,4,3,0,4,3,},
+            new []{9,9,9,3,0,0,0,1,1,0,0,3,},
+            new []{9,9,9,3,0,0,0,0,0,0,0,3,},
+            new []{9,9,9,1,2,2,2,2,2,2,2,1,},
         };
 
         Origin = new Point(0, 0);
@@ -52,7 +60,7 @@ public class Map
             {
                 var cellValue = mapData[y][x];
                 var cellVisual = cellVisuals[cellValue];
-                var cellColor = colorMap[cellValue];
+                var cellColor = GetCellColorByValue(cellValue);
                 Console.ForegroundColor = cellColor;
                 Console.Write(cellVisual);
                 Console.ResetColor();
@@ -67,7 +75,7 @@ public class Map
         {
             if (point.X >= 0 && point.X < mapData[point.Y].Length)
             {
-                if (GetCellAt(point) != 1)
+                if (walkableCellTypes.Contains(GetCellAt(point)))
                 {
                     return true;
                 }
@@ -89,11 +97,16 @@ public class Map
         Console.Write(visual);
     }
 
+    private ConsoleColor GetCellColorByValue(int value)
+    {
+        return colorMap.GetValueOrDefault(value, ConsoleColor.Gray);
+    }
+
     internal void RedrawCellAt(Point position)
     {
         var cellValue = GetCellAt(position);
         var cellVisual = GetCellVisualAt(position);
-        var cellColor = colorMap[cellValue];
+        var cellColor = GetCellColorByValue(cellValue);
 
         Console.ForegroundColor = cellColor;
         DrawSomethingAt(cellVisual, position);
