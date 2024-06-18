c7a8833e4abd5ab0f215557cd9bb51e679a782fa
Konrad Gadzina <konrad@makegames.today>
2024-06-18 11:33:05 +0200

    Clenup old enemy and player code

--- UrbanEureka/Program.cs
+++ UrbanEureka/Program.cs
@@ -1,23 +1,10 @@
 ﻿Console.CursorVisible = false;
 
 Point playerPosition = new Point(10, 3);
-Player hero = new Player("@", playerPosition);
 
 ComposedPlayer composedPlayer = new ComposedPlayer("@", playerPosition);
 
 ComposedEnemy composedEnemy = new ComposedEnemy("T", new Point(4, 4)); 
-
-// Player anotherHero = new Player("Q", new Point(2, 1));
-
-Enemy troll = new Enemy("T", new Point(4, 4));
-
-Character[] characters = new Character[] 
-{
-    hero,
-    // anotherHero,
-    troll,
-    new StaticEnemy("D", new Point(5, 5))
-};
 
 Map map = new Map();
 Point mapOrigin = new Point(15, 3);
@@ -31,10 +18,6 @@ if (map.Size.X + mapOrigin.X >= 0 && map.Size.X + mapOrigin.X < Console.BufferWi
     map.DrawSomethingAt(composedPlayer.VisualComponent.Visual, composedPlayer.PositionComponent.Position);
     map.DrawSomethingAt(composedEnemy.VisualComponent.Visual, composedEnemy.PositionComponent.Position);
 
-    // foreach (var character in characters)
-    // {
-    //     map.DrawSomethingAt(character.Visual, character.Position);
-    // }
 
     while (true)
     {
@@ -55,18 +38,6 @@ if (map.Size.X + mapOrigin.X >= 0 && map.Size.X + mapOrigin.X < Console.BufferWi
             map.RedrawCellAt(composedEnemy.Movement.PreviousPosition);
             map.DrawSomethingAt(composedEnemy.VisualComponent.Visual, composedEnemy.PositionComponent.Position);
         }
-
-        // foreach (var character in characters)
-        // {
-        //     Point nextPosition = character.GetNextPosition();
-        //     if (map.IsPointCorrect(nextPosition))
-        //     {
-        //         character.Move(nextPosition);
-                
-        //         map.RedrawCellAt(character.PreviousPosition);
-        //         map.DrawSomethingAt(character.Visual, character.Position);
-        //     }
-        // }
     }
 }
 else
